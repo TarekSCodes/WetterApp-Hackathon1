f1873f499c05e0d9ff0624fc4f2e35923f8f494d
TarekSCodes <144847405+TarekSCodes@users.noreply.github.com>
2024-07-14 22:23:37 +0200

    Projektdateien hinzufügen.

---
+++ WetterApp/WeatherUI.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WetterApp
+{
+    partial class WeatherUI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WeatherUI));
+            picBoxWeather = new PictureBox();
+            txtBoxCity = new TextBox();
+            lblTemperature = new Label();
+            lblWeatherTxt = new Label();
+            btnCityGo = new Button();
+            lblCity = new Label();
+            ((System.ComponentModel.ISupportInitialize)picBoxWeather).BeginInit();
+            SuspendLayout();
+            // 
+            // picBoxWeather
+            // 
+            picBoxWeather.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            picBoxWeather.BackgroundImageLayout = ImageLayout.Zoom;
+            picBoxWeather.Location = new Point(12, 54);
+            picBoxWeather.Name = "picBoxWeather";
+            picBoxWeather.Padding = new Padding(0, 30, 0, 0);
+            picBoxWeather.Size = new Size(472, 312);
+            picBoxWeather.SizeMode = PictureBoxSizeMode.Zoom;
+            picBoxWeather.TabIndex = 0;
+            picBoxWeather.TabStop = false;
+            // 
+            // txtBoxCity
+            // 
+            txtBoxCity.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtBoxCity.BackColor = Color.White;
+            txtBoxCity.Font = new Font("Segoe UI", 16F);
+            txtBoxCity.Location = new Point(12, 12);
+            txtBoxCity.Name = "txtBoxCity";
+            txtBoxCity.Size = new Size(413, 36);
+            txtBoxCity.TabIndex = 1;
+            txtBoxCity.TextAlign = HorizontalAlignment.Center;
+            txtBoxCity.KeyDown += txtBoxCity_KeyDown;
+            // 
+            // lblTemperature
+            // 
+            lblTemperature.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblTemperature.Font = new Font("Segoe UI", 20F);
+            lblTemperature.Location = new Point(12, 452);
+            lblTemperature.Name = "lblTemperature";
+            lblTemperature.Size = new Size(475, 39);
+            lblTemperature.TabIndex = 4;
+            lblTemperature.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblWeatherTxt
+            // 
+            lblWeatherTxt.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblWeatherTxt.Font = new Font("Segoe UI", 20F);
+            lblWeatherTxt.Location = new Point(12, 413);
+            lblWeatherTxt.Name = "lblWeatherTxt";
+            lblWeatherTxt.Size = new Size(475, 39);
+            lblWeatherTxt.TabIndex = 3;
+            lblWeatherTxt.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnCityGo
+            // 
+            btnCityGo.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnCityGo.BackgroundImage = (Image)resources.GetObject("btnCityGo.BackgroundImage");
+            btnCityGo.BackgroundImageLayout = ImageLayout.Center;
+            btnCityGo.Cursor = Cursors.Hand;
+            btnCityGo.Location = new Point(431, 12);
+            btnCityGo.Name = "btnCityGo";
+            btnCityGo.Size = new Size(53, 36);
+            btnCityGo.TabIndex = 2;
+            btnCityGo.UseVisualStyleBackColor = true;
+            btnCityGo.Click += btnCityGo_Click;
+            // 
+            // lblCity
+            // 
+            lblCity.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblCity.Font = new Font("Segoe UI", 20F);
+            lblCity.Location = new Point(12, 369);
+            lblCity.Name = "lblCity";
+            lblCity.Size = new Size(472, 39);
+            lblCity.TabIndex = 5;
+            lblCity.Text = "Bitte gib einen Ort ein";
+            lblCity.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // WeatherUI
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(496, 527);
+            Controls.Add(lblCity);
+            Controls.Add(btnCityGo);
+            Controls.Add(lblWeatherTxt);
+            Controls.Add(lblTemperature);
+            Controls.Add(txtBoxCity);
+            Controls.Add(picBoxWeather);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "WeatherUI";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "WeatherApp";
+            ((System.ComponentModel.ISupportInitialize)picBoxWeather).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picBoxWeather;
+        private TextBox txtBoxCity;
+        private Label lblTemperature;
+        private Label lblWeatherTxt;
+        private Button btnCityGo;
+        private Label lblCity;
+    }
+}
---
+++ WetterApp/WeatherUI.cs
@@ -0,0 +1,137 @@
+using OpenMeteo;
+
+namespace WetterApp;
+
+public partial class WeatherUI : Form
+{
+    OpenMeteo.OpenMeteoClient mateoClient = new OpenMeteo.OpenMeteoClient();
+    WeatherForecast weatherData = new WeatherForecast();
+
+
+    Dictionary<int, string> weathercodesDic = new Dictionary<int, string>
+    {
+        { 0, "Klarer Himmel" },
+        { 1, "‹berwiegend klar" },
+        { 2, "Teilweise bewˆlkt" },
+        { 3, "Bedeckt" },
+        { 45, "Nebel" },
+        { 48, "Reifnebel" },
+        { 51, "Nieselregen: Leichte Intensit‰t" },
+        { 53, "Nieselregen: M‰ﬂige Intensit‰t" },
+        { 55, "Nieselregen: Dichte Intensit‰t" },
+        { 56, "Gefrierender Nieselregen: Leichte Intensit‰t" },
+        { 57, "Gefrierender Nieselregen: Dichte Intensit‰t" },
+        { 61, "Regen: Leichte Intensit‰t" },
+        { 63, "Regen: M‰ﬂige Intensit‰t" },
+        { 65, "Regen: Starke Intensit‰t" },
+        { 66, "Gefrierender Regen: Leichte Intensit‰t" },
+        { 67, "Gefrierender Regen: Starke Intensit‰t" },
+        { 71, "Schneefall: Leichte Intensit‰t" },
+        { 73, "Schneefall: M‰ﬂige Intensit‰t" },
+        { 75, "Schneefall: Starke Intensit‰t" },
+        { 77, "Schneegriesel" },
+        { 80, "Regenschauer: Leichte Intensit‰t" },
+        { 81, "Regenschauer: M‰ﬂige Intensit‰t" },
+        { 82, "Regenschauer: Heftige Intensit‰t" },
+        { 85, "Schneeschauer: Leichte Intensit‰t" },
+        { 86, "Schneeschauer: Starke Intensit‰t" },
+        { 95, "Leichtes Gewitter" },
+        { 96, "Gewitter mit leichtem Hagel" },
+        { 99, "Gewitter mit starkem Hagel" }
+    };
+
+    public WeatherUI()
+    {
+        InitializeComponent();
+    }
+
+    private async void btnCityGo_Click(object sender, EventArgs e)
+    {
+        weatherData = await mateoClient.QueryAsync(txtBoxCity.Text);
+
+        int weatherCode = GetWeatherCode();
+
+        SetWeatherImage(weatherCode);
+
+        lblCity.Text = "Wetter in " + txtBoxCity.Text;
+
+        if (weathercodesDic.TryGetValue(weatherCode, out var weatherDescription))
+        {
+            lblWeatherTxt.Text = weatherDescription;
+        }
+        else
+        {
+            lblWeatherTxt.Text = "Wetterbeschreibung nicht gefunden.";
+        }
+
+        lblTemperature.Text = weatherData.Current.Temperature + "∞C";
+
+        txtBoxCity.Text = "";
+    }
+
+    /// <summary>
+    /// Detects the pressing of the enter key and executes the btnCityGo_Click() method 
+    /// </summary>
+    private void txtBoxCity_KeyDown(object sender, KeyEventArgs e)
+    {
+
+        if (e.KeyCode == Keys.Enter)
+        {
+            btnCityGo_Click(null, EventArgs.Empty);
+            e.SuppressKeyPress = true;
+        }
+    }
+
+    public int GetWeatherCode()
+    {
+        return weatherData.Current.Weathercode.Value;
+    }
+
+    /// <summary>
+    /// Sets the weather image depending on the weather code
+    /// </summary>
+    /// <param name="weathercode"></param>
+    private void SetWeatherImage(int weathercode)
+    {
+        switch (weathercode)
+        {
+            case 0: picBoxWeather.Image = Properties.Resources.clearsky; break;
+            case 1:
+            case 2:
+            case 3:
+                picBoxWeather.Image = Properties.Resources.partlycloudy; break;
+            case 51:
+            case 53:
+            case 55:
+            case 56:
+            case 57:
+            case 61:
+            case 63:
+            case 65:
+            case 66:
+            case 67:
+            case 80:
+            case 81:
+            case 82:
+                picBoxWeather.Image = Properties.Resources.rain; break;
+            case 95:
+            case 96:
+            case 99:
+                picBoxWeather.Image = Properties.Resources.thunderstorm; break;
+            case 71:
+            case 73:
+            case 75:
+            case 77:
+            case 85:
+            case 86:
+                picBoxWeather.Image= Properties.Resources.snow; break;
+
+            default: picBoxWeather.Image = null; break;
+        }
+    }
+} 
+/*
+WMO Weather interpretation codes (WW)
+Code	Description
+45, 48	Fog and depositing rime fog
+*/
